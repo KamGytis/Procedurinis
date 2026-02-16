2696972b52f91d1f7d8ee1d73f2f43d9f508559d
Gytis Kaminskas <gytisskaminskas@gmail.com>
2026-02-16 21:57:30 +0200

    pradine versija

--- main.cpp
+++ main.cpp
@@ -9,45 +9,44 @@ struct Student
     std::string vardas = "A";
     std::string pavarde = "BB";
 
-    std::vector<int> paz = {0}; 
+    std::vector<int> paz = { 0 };
     int egz;
-    double rez;   
+    double rez;
+
 };
 
- using namespace std;
+using namespace std;
 int main()
 {
     Student A;
     vector<Student> grupe;
-    for(int ii=0; ii<2; ii++)
+    for (int ii = 0; ii < 2; ii++)
     {
-        cout<<"Iveskite varda or pavarde: ";
-        cin>>A.vardas>>A.pavarde;
-        //std::cout<<A.vardas<<" "<<A.pavarde<<std::endl;
-        cout<<"Iveskite semestro pazymius:\nKiek pazymiu: ";
-        int n, temp, sum=0;
-        cin>>n;
-        for(int i=0; i<n; i++)
-            {
-            cout<<"Iveskite "<<i+1<<" pazymi: "<<n<<":"<<endl;
-            cin>>temp;
+        cout << "Iveskite varda or pavarde: ";
+        cin >> A.vardas >> A.pavarde;
+        cout << "Iveskite semestro pazymius:\nKiek pazymiu: ";
+        int n, temp, sum = 0;
+        cin >> n;
+        for (int i = 0; i < n; i++)
+        {
+            cout << "Iveskite " << i + 1 << " pazymi: " << n << ":" << endl;
+            cin >> temp;
             A.paz.push_back(temp);
-            sum+=temp;
-            }
+            sum += temp;
+        }
         A.rez = sum / (double)n;
-       
-    cout<<"Iveskite egzamino pazymi: "<<endl;
-    cin>>A.egz;
-    A.rez = (sum / n) * 0.4 + A.egz * 0.6;
-    grupe.push_back(A);
-    A.paz.clear();
+        cout << "Iveskite egzamino pazymi: " << endl;
+        cin >> A.egz;
+        A.rez = (sum / n) * 0.4 + A.egz * 0.6;
+        grupe.push_back(A);
+        A.paz.clear();
     }
-    for(auto A : grupe)
+    for (auto A : grupe)
     {
-        std::cout<<std::left<<setw(10)<<A.vardas<<std::right<<setw(10)<<A.pavarde<<endl;
-        //for(auto a:A.paz) cout<<setw(3)<<a;
-        //cout<<setw(10)<<A.egz<<endl;
-        cout<<setw(20)<<A.rez<<endl;
+        std::cout << std::left << setw(10) << A.vardas << std::right << setw(10) << A.pavarde << endl;
+        for (auto a : A.paz) cout << setw(3) << a;
+        cout << setw(10) << A.egz << endl;
+        cout << setw(20) << A.rez << endl;
     }
     return 0;
 }
